03930131680d450d268821f11c8be53bd2644139
tmtowtdi <tmtowtdi@gmail.com>
2013-07-23 15:35:05 -0400

    Fixed use statements in rescue_lost_window.pl (derp)

--- bin/rescue_lost_window.pl
+++ bin/rescue_lost_window.pl
@@ -3,31 +3,53 @@
 use v5.14;
 use FindBin;
 use lib $FindBin::Bin . "/../lib";
-use LacunaWaX::Container;
-use LacunaWaX::Schema;
+say $FindBin::Bin . "/../lib";
+use LacunaWaX::Model::Container;
+use LacunaWaX::Model::Schema;
 use version;
-
-=pod
-
-=cut
 
 
 my $root_dir    = "$FindBin::Bin/..";
 my $db_file     = $root_dir . '/user/lacuna_app.sqlite';
 my $db_log_file = $root_dir . '/user/lacuna_log.sqlite';
 
+=pod
+
+Careful about adding text below.  The output is arranged so the "Hit 
+<ENTER>..." prompts will display on the last line of a CMD window; 
+adding text will likely move those prompts down.
+
+So if you do add text, be sure to run this IN A CMD WINDOW NOT IN YOUR 
+RESIZED Console, DUMMY to make sure that everything shows up where it 
+should.
+
+=cut
 
 
 clear_screen();
 say <<EOT;
 
-If you started LacunaWaX, and can see its icon running in your task bar, but 
-can't find the actual LacunaWaX window, this will reset its position for you.
 
-First, be sure to close down LacunaWaX if it's running - right-click on its 
-button in the task bar and chose "Close".
 
-You must be sure that LacunaWaX is not running before we continue.
+    If you started LacunaWaX, and can see its icon running in your 
+    task bar, but can't find the actual LacunaWaX window, this will 
+    reset its position for you.
+
+
+
+    First, be sure to close down LacunaWaX if it's running - 
+    right-click on its button in the task bar and chose "Close".
+
+    Next, start the Windows Task Manager - if you see any instances of 
+    LacunaWaX.exe running, end them.
+
+
+
+    You must be sure that LacunaWaX is not running before we continue.
+
+
+
+
 
 EOT
 
@@ -37,7 +59,7 @@ my $h = <STDIN>;
 
 
 
-my $c = LacunaWaX::Container->new(
+my $c = LacunaWaX::Model::Container->new(
     name        => 'my container',
     root_dir    => $root_dir,
     db_file     => $db_file,
@@ -55,10 +77,10 @@ if( my $y = $schema->resultset('AppPrefsKeystore')->search({ name => 'MainWindow
 clear_screen();
 say <<EOT;
 
-Reset complete!
+    Reset complete!
 
-You should be all set.  The next time you start LacunaWaX, its window should 
-start out in the center of your screen.
+    You should be all set.  The next time you start LacunaWaX, its 
+    window should start out in the center of your screen.
 
 
 
